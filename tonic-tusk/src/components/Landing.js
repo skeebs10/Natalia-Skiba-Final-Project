3398a95f0b85ed9b15da76d42ff0e98b1fdfce98
Natalia Skiba <nataliaskiba@Natalias-MacBook-Pro.local>
2021-03-24 22:00:13 -0500

    Navbar

--- tonic-tusk/src/components/Landing.js
+++ tonic-tusk/src/components/Landing.js
@@ -9,50 +9,50 @@ import "slick-carousel/slick/slick-theme.css";
 function Landing() {
     const [count, setCount] = useState(0);
     setTimeout(() => {
-      if (count === 1) {
+    if (count === 1) {
         setCount(0);
-      } else {
+    } else {
         setCount(count + 1);
-      }
+    }
     }, 5000);
-  
+
 
 
 const images = [
     {
-      img:
+    img:
         "https://i0.wp.com/bowlofgoodness.com/wp-content/uploads/2016/09/happy-fit-people.jpg?resize=770%2C369",
-      text: "#Good Health, Good Cause",
+    text: "#Good Health, Good Cause",
     },
     {
-      img:
+    img:
         "https://s3-ap-south-1.amazonaws.com/soulveda-media-prod/wp-content/uploads/2018/05/22171103/1523019875.banner.Good-health-beyond-the-body-776x620.jpg",
-      text: "#Save Elephants",
+    text: "#Save Elephants",
     },
-  ];
-  const icons = [
+];
+const icons = [
     {
-      img:
+    img:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-0VsgtqiWi_C1BGM4bhp-UFM6T0XBw6sDSA&usqp=CAU",
-      text: "THIRD PARTY TESTED",
+    text: "THIRD PARTY TESTED",
     },
     {
-      img:
+    img:
         "https://cdn0.iconfinder.com/data/icons/tidee-health/24/015_005_heart_health_good_healthy-512.png",
-      text: "ZERO HIGH",
+    text: "ZERO HIGH",
     },
     {
-      img: "https://static.thenounproject.com/png/2405330-200.png",
-      text: "TRAVEL FRIENDLY",
+    img: "https://static.thenounproject.com/png/2405330-200.png",
+    text: "TRAVEL FRIENDLY",
     },
     {
-      img:
+    img:
         "https://cdn2.iconfinder.com/data/icons/medical-bundle-1/64/Heart_-512.png",
-      text: "STRESS LESS",
+    text: "STRESS LESS",
     },
-  ];
+];
 
-  const setting = {
+const setting = {
     dots: true,
     focusOnSelect: true,
     infinite: true,
@@ -64,39 +64,37 @@ const images = [
     slidesToScroll: 1,
 };
 const Slide = () => {
-  return (
+return (
     <div className="slide__container">
-      <img src="https://res.cloudinary.com/dd77cqt5fs/image/upload/v1616548015/bottle_gdcgl0.png"></img>
-      <div className="content__container">
+    <img src="https://res.cloudinary.com/dd77cqt5fs/image/upload/v1616548015/bottle_gdcgl0.png"></img>
+    <div className="content__container">
         <h3 className="slide__title">Tonic Tusk CBD Gum</h3>
         <p className="price">$350</p>
-        <div className="d-flex">
-          <h6 className="quantity">Quantity</h6>
-          <p className="product__quantity">1</p>
+            <div className="d-flex">
+            <h6 className="quantity">Quantity</h6>
+            <p className="product__quantity">1</p>
         </div>
-        <NavLink to="#" className="order__button">
-          ORDER NOW
-        </NavLink>
+        <NavLink to="#" className="order__button">ORDER NOW </NavLink>
         <div className="d-flex justify-content-center">
-          <p className="payment">More Payment Options</p>
+            <p className="payment">More Payment Options</p>
         </div>
         <p className="desc1">
-          Our functional yet sustainable water bottle acts as a visual
-          reminder to stay hydrated. Drinking more water gets rid of fatigue,
-          boosts your metabolism, clears your skin, and helps with better
-          concentration.
+            Our functional yet sustainable water bottle acts as a visual
+            reminder to stay hydrated. Drinking more water gets rid of fatigue,
+            boosts your metabolism, clears your skin, and helps with better
+            concentration.
         </p>
         <h6 className="desc__title">Benefits</h6>
         <pre className="points">
-          {" "}
-          1. Time stamps to help you track your progress of daily water
-          intake.<br></br> 2. BFA-free and lightweight for easy carry.
-          <br></br> 3. 1L of water per fill for plenty of hydration.
-          <br></br> 4. Beautiful and thoughtful design.
+            {" "}
+            1. Time stamps to help you track your progress of daily water
+            intake.<br></br> 2. BFA-free and lightweight for easy carry.
+            <br></br> 3. 1L of water per fill for plenty of hydration.
+            <br></br> 4. Beautiful and thoughtful design.
         </pre>
-      </div>
     </div>
-  );
+    </div>
+    );
 };
 
 
---
+++ tonic-tusk/src/components/Navbar.js
@@ -0,0 +1,3 @@
+import React, {UseState} from "react";
+import {AiOutlineSearch} from "react-cons/ai";
+import { BiCartAtl } from "react-icons/bi"
