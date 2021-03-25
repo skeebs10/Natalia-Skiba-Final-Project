6a04ced1d2ea31d99e61ef1f1d9214452d90db7d
Natalia Skiba <nataliaskiba@Natalias-MacBook-Pro.local>
2021-03-24 22:11:34 -0500

    add to Navbar

--- tonic-tusk/src/components/Navbar.js
+++ tonic-tusk/src/components/Navbar.js
@@ -1,3 +1,48 @@
 import React, {UseState} from "react";
 import {AiOutlineSearch} from "react-cons/ai";
-import { BiCartAtl } from "react-icons/bi"
+import { BiCartAtl } from "react-icons/bi";
+
+function Navbar() {
+    cont [nav, setnav] = useState(false);
+    const setback = () => {
+        if (window.scrollY >= 70) {
+            setnav(true);
+        }  else {
+            setnav(false);
+        }
+    };
+
+    window.addEventListener("scroll", setback);
+    
+    return (
+        <div
+            className={
+            nav === true
+                ? "navbar active container-fluid m-0 p-0"
+                : "navbar container-fluid m-0 p-0"
+            }
+            style={{ position: "sticky", zIndex: "3" }}
+            fixed="top"
+        >
+            <img src="https://res.cloudinary.com/dd77cqt5fs/image/upload/v1616531620/tonic_mdakf5.png"></img>
+            <div>
+        <div>
+          <h3 className="change">Shop</h3>
+          <h3 className="change">Our mission</h3>
+          <h3 className="change">News</h3>
+          <h3 className="change">Contact</h3>
+        </div>
+        <div className="icons">
+          <p className="search change">
+            <AiOutlineSearch />
+          </p>
+          <p className="cart change">
+            <BiCartAlt />
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
