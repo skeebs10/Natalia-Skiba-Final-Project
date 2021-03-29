218788f342963419fde6a361659462975aa36ee2
Natalia Skiba <nataliaskiba@Natalias-MacBook-Pro.local>
2021-03-28 23:05:04 -0500

    shop.js file

--- tonic-tusk/src/components/Landing.js
+++ tonic-tusk/src/components/Landing.js
@@ -1,42 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import "../css/landing.scss";
 import { NavLink } from "react-router-dom";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Footer from "./Footer";
+import { useDispatch, useSelector } from "react-redux";
+import { AllProduct } from "../actions/allproducts";
 
 function Landing() {
     const [count, setCount] = useState(0);
+    const dispatch = useDispatch();
+    useEffect(() => {
+    dispatch(AllProduct());
+    }, []);
     setTimeout(() => {
-        if (count === 1) {
+    if (count === 1) {
         setCount(0);
-        } else {
-            setCount(count + 1);
-        }
+    } else {
+        setCount(count + 1);
+    }
     }, 5000);
+    const allP = useSelector((state) => state.allP);
 
     const images = [
     {
         img:
-            "https://i0.wp.com/bowlofgoodness.com/wp-content/uploads/2016/09/happy-fit-people.jpg?resize=770%2C369",
+        "https://i0.wp.com/bowlofgoodness.com/wp-content/uploads/2016/09/happy-fit-people.jpg?resize=770%2C369",
         text: "#Good Health, Good Cause",
     },
     {
         img:
-            "https://s3-ap-south-1.amazonaws.com/soulveda-media-prod/wp-content/uploads/2018/05/22171103/1523019875.banner.Good-health-beyond-the-body-776x620.jpg",
+        "https://s3-ap-south-1.amazonaws.com/soulveda-media-prod/wp-content/uploads/2018/05/22171103/1523019875.banner.Good-health-beyond-the-body-776x620.jpg",
         text: "#Save Elephants",
     },
     ];
     const icons = [
     {
         img:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-0VsgtqiWi_C1BGM4bhp-UFM6T0XBw6sDSA&usqp=CAU",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-0VsgtqiWi_C1BGM4bhp-UFM6T0XBw6sDSA&usqp=CAU",
         text: "THIRD PARTY TESTED",
     },
     {
         img:
-            "https://cdn0.iconfinder.com/data/icons/tidee-health/24/015_005_heart_health_good_healthy-512.png",
+        "https://cdn0.iconfinder.com/data/icons/tidee-health/24/015_005_heart_health_good_healthy-512.png",
         text: "ZERO HIGH",
     },
     {
@@ -49,162 +57,153 @@ function Landing() {
         text: "STRESS LESS",
     },
     ];
-
     const setting = {
-        dots: true,
-        focusOnSelect: true,
-        infinite: true,
-        autoplay: true,
-        centerMode: false,
-        swipe: false,
-        autoplaySpeed: 3000,
-        slidesToShow: 1,
-        slidesToScroll: 1,
+    dots: true,
+    focusOnSelect: true,
+    infinite: true,
+    autoplay: true,
+    centerMode: false,
+    swipe: false,
+    autoplaySpeed: 3000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
     };
 
-    const Slide = () => {
     return (
-        <div className="slide__container">
-            <img src="https://res.cloudinary.com/dd77cqt5fs/image/upload/v1616548015/bottle_gdcgl0.png"></img>
-            <div className="content__container">
-                <h3 className="slide__title">Tonic Tusk CBD Gum</h3>
-                <p className="price">$350</p>
-                <div className="d-flex">
-                    <h6 className="quantity">Quantity</h6>
-                <p className="product__quantity">1</p>
-                </div>
-                <NavLink to="#" className="order__button">
-                    ORDER NOW
-                </NavLink>
-                <div className="d-flex justify-content-center">
-                    <p className="payment">More Payment Options</p>
-                </div>
-                <p className="desc1">
-                    Our functional yet sustainable water bottle acts as a visual
-                    reminder to stay hydrated. Drinking more water gets rid of fatigue,
-                    boosts your metabolism, clears your skin, and helps with better
-                    concentration.
-                </p>
-                <h6 className="desc__title">Benefits</h6>
-                <pre className="points">
-                    {" "}
-                    1. Time stamps to help you track your progress of daily water
-                    intake.<br></br> 2. BFA-free and lightweight for easy carry.
-                    <br></br> 3. 1L of water per fill for plenty of hydration.
-                    <br></br> 4. Beautiful and thoughtful design.
-                </pre>
-            </div>
-        </div>
-    );
-};
-
-return (
     <div className="container-fluid m-0 p-0">
         <Navbar />
         <div className="section1">
-            <img src={images[count].img}></img>
-            <h1>{images[count].text}</h1>
+        <img src={images[count].img}></img>
+        <h1>{images[count].text}</h1>
         </div>
         <div className="section2">
         {icons.map((icon) => {
             return (
             <div className="icon__container">
                 <div>
-                    <img src={icon.img}></img>
-                    <p>{icon.text}</p>
+                <img src={icon.img}></img>
+                <p>{icon.text}</p>
                 </div>
             </div>
-        );
+            );
         })}
         </div>
         <div className="cont">
-            <div className="product__section">
-                <div className="title">
-                    <h1>#Trendy Products</h1>
-                </div>
-                <div className="cont">
-                    <Slider {...setting}>
-                        <Slide />
-                        <Slide />
-                    </Slider>
-                </div>
+        <div className="product__section">
+            <div className="title">
+            <h1>#Trendy Products</h1>
             </div>
+            <div className="cont">
+            <Slider {...setting}>
+                {allP.products.map((prod) => {
+                return (
+                    <div>
+                    <div className="slide__container">
+                        <img src={prod.productImage[0].img}></img>
+                        <div className="content__container">
+                        <h3 className="slide__title">{prod.name}</h3>
+                        <p className="price">${prod.price}</p>
+                        <div className="d-flex">
+                            <h6 className="quantity">Quantity</h6>
+                            <p className="product__quantity">1</p>
+                        </div>
+                        <NavLink
+                            to={`/product/${prod._id}`}
+                            className="order__button"
+                        >
+                            ORDER NOW
+                        </NavLink>
+                        <div className="d-flex justify-content-center">
+                            <p className="payment">More Payment Options</p>
+                        </div>
+                        <p className="desc1">{prod.description}</p>
+                        <h6 className="desc__title">Benefits</h6>
+                        <pre className="points">{prod.benefits}</pre>
+                        </div>
+                    </div>
+                    </div>
+                );
+                })}
+            </Slider>
+            </div>
+        </div>
         </div>
         <div className="section4">
-            <div className="d-flex justify-content-center">
-                <img
-                    src="https://res.cloudinary.com/dd77cqt5fs/image/upload/v1616553573/commas_lvog09.png"
-                    className="section4__image"
-                ></img>
-            </div>
-            <div className="d-flex justify-content-center">
-                <h1 className="section4__title">SAVE THE ELEPHANTS</h1>
-            </div>
-            <div className="d-flex justify-content-center">
-                <h1 className="section4__title">
-                    Every 15 minutes an elephant is killed for its ivory tusks
-                </h1>
-            </div>
+        <div className="d-flex justify-content-center">
+            <img
+            src="https://res.cloudinary.com/dd77cqt5fs/image/upload/v1616553573/commas_lvog09.png"
+            className="section4__image"
+            ></img>
+        </div>
+        <div className="d-flex justify-content-center">
+            <h1 className="section4__title">SAVE THE ELEPHANTS</h1>
+        </div>
+        <div className="d-flex justify-content-center">
+            <h1 className="section4__title">
+            Every 15 minutes an elephant is killed for its ivory tusks
+            </h1>
+        </div>
         </div>
         <div className="section5">
-            <div className="sub_section1">
-                <div className="d-flex justify-content-center">
-                    <h1 className="title">OUR GOAL: MAKE A DiFFERENCE</h1>
+        <div className="sub_section1">
+            <div className="d-flex justify-content-center">
+            <h1 className="title">OUR GOAL: MAKE A DiFFERENCE</h1>
             </div>
             <p className="description1">
-                10% of every purchase is donated to the
+            10% of every purchase is donated to the
             </p>
             <p className="description2">
-                ELEPHANT Crises fund | The Leonardo DiCaprio Foundation
+            ELEPHANT Crises fund | The Leonardo DiCaprio Foundation
             </p>
         </div>
         <div className="sub_section2">
             <img
-                src="https://res.cloudinary.com/dd77cqt5fs/image/upload/v1616554973/elephant_jt3mbi.png"
-                className="ele"
+            src="https://res.cloudinary.com/dd77cqt5fs/image/upload/v1616554973/elephant_jt3mbi.png"
+            className="ele"
             ></img>
         </div>
-    </div>
-    <div className="section6">
+        </div>
+        <div className="section6">
         <div className="sub__section1">
             <img
-                src={
-                    "https://res.cloudinary.com/dd77cqt5fs/image/upload/v1616565552/aa-removebg-preview_l63nrx.png"
-                }
-                className="logo"
+            src={
+                "https://res.cloudinary.com/dd77cqt5fs/image/upload/v1616565552/aa-removebg-preview_l63nrx.png"
+            }
+            className="logo"
             ></img>
             <div className="text-section">
-                <p className="text">
-                    High quality CBD for the on-the-go lifestyle! Love these products!
-                    <p className="text2">-Alexa. A</p>
-                </p>
+            <p className="text">
+                High quality CBD for the on-the-go lifestyle! Love these products!
+                <p className="text2">-Alexa. A</p>
+            </p>
             </div>
         </div>
         <div className="sub__section2">
             <img
-                src={
-                    "https://res.cloudinary.com/dd77cqt5fs/image/upload/v1616565552/aa-removebg-preview_l63nrx.png"
-                }
-                className="logo"
+            src={
+                "https://res.cloudinary.com/dd77cqt5fs/image/upload/v1616565552/aa-removebg-preview_l63nrx.png"
+            }
+            className="logo"
             ></img>
             <div className="text-section">
-                <p className="text">
-                    A great way to keep hygene stress free while supporting the fight
-                    to end ivory tusk poaching.
-                    <p className="text2">-Sebastian S.</p>
-                </p>
+            <p className="text">
+                A great way to keep hygene stress free while supporting the fight
+                to end ivory tusk poaching.
+                <p className="text2">-Sebastian S.</p>
+            </p>
             </div>
         </div>
-    </div>
-    <div className="section7">
+        </div>
+        <div className="section7">
         <div className="con">
             <img src="https://res.cloudinary.com/dd77cqt5fs/image/upload/v1616565274/circle-cropped__5_-removebg-preview_ms35vy.png"></img>
         </div>
         <div className="con2">
             <h1 className="logo-text">Elephant Crisis Fund</h1>
         </div>
-    </div>
-    <div className="section8"></div>
+        </div>
+
+        <Footer />
     </div>
     );
 }
--- tonic-tusk/src/components/Navbar.js
+++ tonic-tusk/src/components/Navbar.js
@@ -1,43 +1,52 @@
 import React, { useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { BiCartAlt } from "react-icons/bi";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
     const [nav, setnav] = useState(false);
-    const setback = () => {  
-        if (window.scrollY >= 70) {
-            setnav(true);
-    }   else {
+    const setback = () => {
+    if (window.scrollY >= 70) {
+        setnav(true);
+    } else {
         setnav(false);
     }
-};
+    };
     window.addEventListener("scroll", setback);
     return (
     <div
         className={
-            nav === true
+        nav === true
             ? "navbar active container-fluid m-0 p-0"
             : "navbar container-fluid m-0 p-0"
         }
         style={{ position: "sticky", zIndex: "3" }}
         fixed="top"
-        >
+    >
         <img src="https://res.cloudinary.com/dd77cqt5fs/image/upload/v1616531620/tonic_mdakf5.png"></img>
         <div>
-            <div>
-                <h3 className="change">Shop</h3>
-                <h3 className="change">Our mission</h3>
-                <h3 className="change">News</h3>
-                <h3 className="change">Contact</h3>
-            </div>
-            <div className="icons">
-                <p className="search change">
-                    <AiOutlineSearch />
-                </p>
-                <p className="cart change">
-                    <BiCartAlt />
-                </p>
-            </div>
+        <div>
+            <NavLink to="/shop" className="change lo">
+            Shop
+            </NavLink>
+            <NavLink to="/shop" className="change lo">
+            Our mission
+            </NavLink>
+            <NavLink to="/shop" className="change lo">
+            News
+            </NavLink>
+            <NavLink to="/shop" className="change lo">
+            Contact
+            </NavLink>
+        </div>
+        <div className="icons">
+            <p className="search change">
+            <AiOutlineSearch />
+            </p>
+            <p className="cart change">
+            <BiCartAlt />
+            </p>
+        </div>
         </div>
     </div>
     );
