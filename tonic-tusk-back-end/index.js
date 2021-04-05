14d5dbe5a3742428272fb76f3a71ca381df92d66
Natalia Skiba <nataliaskiba@Natalias-MacBook-Pro.local>
2021-04-05 13:58:19 -0500

    index.js

--- tonic-tusk-back-end/index.js
+++ tonic-tusk-back-end/index.js
@@ -2,21 +2,26 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-const userRoute = require("./Routes/user");
+const userRoute = require("../backend/Routes/user");
 const productRoute = require("./Routes/products");
 const cartRoute = require("./Routes/cart");
+const paymentRoute = require("./Routes/payment");
+const orderroutes = require("./Routes/order");
 const path = require("path");
 
 app.use(express.json());
 app.use(cors());
 
 mongoose
-    .connect("mongodb+srv://natalia:L4jAjfgEGkt0mAYO@cluster0.9yvku.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    })
+    .connect(
+    "mongodb+srv://natalia:L4jAjfgEGkt0mAYO@cluster0.9yvku.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    }
+    )
     .then((res) => {
     console.log("conected");
     })
@@ -27,5 +32,7 @@ app.use("/public", express.static(path.join(__dirname, "uploads")));
 app.use(userRoute);
 app.use(productRoute);
 app.use(cartRoute);
+app.use(paymentRoute);
+app.use(orderroutes);
 
-app.listen("8000", console.log("listening to port 8000"));
+app.listen("8000", console.log("listening to port 8080"));
