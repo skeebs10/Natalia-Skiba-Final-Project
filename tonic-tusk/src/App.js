b36d77a06e288e7aba570733ed1a6fe9da337187
Natalia Skiba <nataliaskiba@Natalias-MacBook-Pro.local>
2021-04-05 13:36:59 -0500

    updated App.js

--- tonic-tusk/src/App.js
+++ tonic-tusk/src/App.js
@@ -2,9 +2,11 @@ import { Switch, Route } from "react-router";
 import About from "./components/About";
 import Cart from "./components/Cart";
 import Contact from "./components/Contact";
+import GuestPayment from "./components/GuestPayment";
 import Landing from "./components/Landing";
 import Product from "./components/Product";
 import Shop from "./components/Shop";
+import Confirmation from "./components/OrderConfirmation";
 
 function App() {
   return (
@@ -16,6 +18,11 @@ function App() {
         <Route path="/shop" component={Shop}></Route>
         <Route path="/about" component={About}></Route>
         <Route path="/contact" component={Contact}></Route>
+        <Route
+          path="/guestpayment/:Cid/:price"
+          component={GuestPayment}
+        ></Route>
+        <Route path="/confirmation" component={Confirmation}></Route>
       </Switch>
     </div>
   );
